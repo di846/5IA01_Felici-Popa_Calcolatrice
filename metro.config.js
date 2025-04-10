1fcb2b315570e832a57cd506b0bd51f205fad586
di846 <felicifilippo8@gmail.com>
2025-04-10 22:58:58 +0200

    aggiunto metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...defaultConfig,
+  resolver: {
+    extraNodeModules: {
+      'stream': require.resolve('readable-stream'),
+      // Add other necessary polyfills here
+    },
+  },
+};
